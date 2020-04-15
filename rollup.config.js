979a232be9ca48e11899e20d2126fd620c5690f9
Arun Srinivasan <rulfzid@gmail.com>
2020-04-15 10:30:14 -0500

    ESMify this package, following suit with the other lezer parsers
    See, for example:
    - https://github.com/lezer-parser/css/commit/ceace4d8588debd983eaefc65502220c94667d74
    - https://github.com/lezer-parser/css/commit/b3dc69b4925fee829ec0c44e346b61ae2213f6d7

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import nodeResolve from "rollup-plugin-node-resolve"
 export default {
   input: "./src/parser.js",
   output: [
-    { format: "cjs", file: "./dist/index.js" },
+    { format: "cjs", file: "./dist/index.cjs" },
     { format: "es", file: "./dist/index.es.js" }
   ],
   external(id) {
